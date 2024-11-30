4dab47ac7cf1cd1100641690fc5bfc147093601b
Lav <lavhuyaar@gmail.com>
2024-11-30 22:28:52 +0530

    feat: Products array created; components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const NUMBER_OF_PRODUCTS: number = 12;
 
+  interface Product {
+    id: number;
+    title: string;
+    price: string;
+    description: string;
+    image: string;
+  }
 
-  return (
-    <>
-     
-    </>
-  )
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(
+        `https://fakestoreapi.com/products?limit=${NUMBER_OF_PRODUCTS}`
+      );
+      const data: Product[] = await response.json();
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
+  return( <>
+
+  <h1>Hello dostooooo</h1>
+  </>)
 }
 
-export default App
+export default App;
