984b63c4bb72ca36f78e227e878292eacbb07618
Lav <lavhuyaar@gmail.com>
2024-12-02 09:10:45 +0530

    feat: Product page designed

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Link, useOutletContext } from "react-router-dom";
 import { OutletContextType, Product } from "../../types/types";
+import { useState } from "react";
 
 type ProductCardProps = {
   product: Product;
@@ -8,9 +9,17 @@ type ProductCardProps = {
 function ProductCard({ product }: ProductCardProps) {
   const { handleAddToCart } = useOutletContext<OutletContextType>();
 
+  const [quantity, setQuantity] = useState(1);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = Number(event.target.value);
+      setQuantity(value);
+      console.log(value)
+  }
+
   return (
     <>
-      <div className="product text-white bg-[#242424] flex flex-col align-center p-3 gap-1 rounded-xl">
+      <div className="product text-white bg-[#242424] flex flex-col align-center p-3 gap-2 rounded-xl">
         <Link to={`/shop/products/${product.id}`}>
           <div className=" product-img-container object-cover w-full h-[230px] flex items-center justify-center bg-white rounded-xl">
             <img
@@ -29,7 +38,10 @@ function ProductCard({ product }: ProductCardProps) {
         <h3 className="font-bold pt-3 whitespace-nowrap overflow-hidden text-ellipsis text-xl text-white">
           {product.title}
         </h3>
-        <div className="text-lg">Price: {product.price} $</div>
+        <div className="text-lg flex gap-8 items-center ">
+          <p>Price: {product.price} $</p>
+          <input type="number" value={quantity} onChange={handleOnChange} className="text-[#242424] px-1 py-0.5 w-8 rounded-sm text-sm text-bold"/>
+          </div>
         <button
           type="button"
           className="p-2  bg-[#BB86F6] mt-2 text-lg font-bold text-[#242424] rounded-xl hover:bg-[#a063e6]"
--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useOutletContext } from "react-router-dom";
 import { OutletContextType } from "../../types/types";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 function ProductPage() {
   const { id } = useParams<{ id: string }>();
-  const {products} = useOutletContext<OutletContextType>();
+  const {products, handleAddToCart} = useOutletContext<OutletContextType>();
   const product = products.find((p) => p.id === Number(id));
+
 
   if (!product) {
     return <p>Product not found!</p>;
@@ -13,9 +16,38 @@ function ProductPage() {
 
   return (
     <>
-      <main className="bg-green-400 min-h-[90vh]">
-        <div>{product.title}</div>
-        lol product page
+      <main className="bg-[#1F1B24] min-h-[90vh] text-white" >
+
+      <Link to="/shop">
+        <FaArrowLeft style={{width: '40px', height: '40px',position: 'relative', left: '40px', top: '20px', marginBottom: '10px'}}/>
+      </Link>
+
+        <div className="w-full flex items-center mt-16 p-8 justify-center gap-8">
+        <div className="flex justify-center items-center w-[500px] bg-white rounded-xl object-cover">
+          <img src={product.image} alt={product.title}  style={{
+                objectPosition: "center",
+                objectFit: "contain",
+                height: "90%",
+                width: "90%",
+                padding: '10px'
+              }} />
+        </div>
+        <section className="flex flex-col gap-4">
+          <h1 className="text-5xl font-bold">{product.title}</h1>
+          <article>
+              <p className="text-sm">{product.description}</p>
+          </article>
+              <div className="text-xl">Price: {product.price} $</div>
+              <button
+          type="button"
+          className="p-2  bg-[#BB86F6] mt-2 text-2xl font-bold text-[#242424] rounded-xl hover:bg-[#a063e6] px-10 py-4 w-[300px]"
+          onClick={() => handleAddToCart(product)}
+        >
+          Add to cart
+        </button>
+        </section>
+        </div>
+        
       </main>
     </>
   );
